664a8eb011a7b80f1aa8ab5ad16fc8ff9494b22a
Mikhail Belozerov <zigawick@gmail.com>
2017-08-17 20:22:59 +0300

    Slot logic changed

--- entities/slot.go
+++ entities/slot.go
@@ -3,17 +3,18 @@ package entities
 import "errors"
 
 type Slot struct {
-	name         string
 	capacity     int
+	contains     int
 	isAccessible bool
-	item         *Item
+	name         string
+	items        map[int][]*Item
 }
 
 func NewSlot(name string, capacity int, isAccessible bool) *Slot {
 	return &Slot{
-		name:         name,
 		capacity:     capacity,
-		item:         nil,
+		contains:     0,
+		name:         name,
 		isAccessible: isAccessible,
 	}
 }
@@ -30,30 +31,50 @@ func (s *Slot) Name() string {
 	return s.name
 }
 
-func (s *Slot) Empty() bool {
-	return s.item == nil
-}
-
-func (s *Slot) MoveTo(another *Slot) error {
-	if s.Empty() {
-		return errors.New("Origin slot is empty")
-	}
-	var putErr = another.canPut(s.item)
-	if putErr != nil {
-		return putErr
-	}
-
-	another.item = s.item
-	s.item = nil
-	return nil
-}
-
 func (s *Slot) canPut(item *Item) error {
 	if !s.isAccessible {
 		return errors.New("Can slot is not open")
 	}
-	if item.Size > s.capacity {
+	if item.Size+s.contains > s.capacity {
 		return errors.New("Item is too big")
 	}
+	return nil
+}
+
+func (s *Slot) hasItem(id int) error {
+	if !s.isAccessible {
+		return errors.New("Can slot is not open")
+	}
+
+	_, err := s.items[id]
+
+	if !err {
+		return errors.New("No such item in this slot")
+	}
+
+	return nil
+}
+
+func (s *Slot) getItem(id int) (item *Item, err error) {
+	err = s.hasItem(id)
+	if err != nil {
+		return nil, err
+	}
+
+	item = s.items[id][len(s.items[id])-1]
+	s.items[id] = s.items[id][:len(s.items)-2]
+	s.contains -= item.Size
+	return item, nil
+}
+
+func (s *Slot) putItem(item *Item) error {
+	err := s.canPut(item)
+	if err != nil {
+		return err
+	}
+
+	s.contains += item.Size
+	s.items[item.Id] = append(s.items[item.Id], item)
+
 	return nil
 }
