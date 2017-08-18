ea8b0479a48dde6f64c83385a72df5c7060e7721
Shchurbin Egor <rentgen.94@yandex.ru>
2017-08-18 16:43:27 +0300

    add Simple HttpServer(Auth & Register)

---
+++ services/DBWork.go
@@ -0,0 +1,40 @@
+package services
+
+
+import (
+	_ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	PostgresUser = "postgres"
+	Password = "qwerty456"
+	Server = "localhost:5432"
+	DbName = "testdb"
+	Schema = "QuestGoMail"
+)
+
+var db *sql.DB
+
+func init() {
+	DB, err := sql.Open("postgres",
+		"postgres://"+PostgresUser+":"+Password+"@"+Server+"/"+DbName+"?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = DB
+
+	setSCH, err := db.Query("SET SCHEMA '" + Schema + "';")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer setSCH.Close()
+
+	for setSCH.Next() {
+		fmt.Println(setSCH.Scan())
+	}
+
+	ShowAllUser()
+}
---
+++ services/UserDAO.go
@@ -0,0 +1,72 @@
+package services
+
+import (
+
+	"log"
+	"fmt"
+	"net/http"
+	"database/sql"
+)
+
+type User struct {
+	Id       int
+	Name     string
+	Password string
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request, name, pass string) {
+	_, err := db.Exec("INSERT INTO users(name, password) VALUES ($1, $2);", name, pass)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	//rowsAffected, err := result.RowsAffected()
+	//if err != nil {
+	//	http.Error(w, http.StatusText(500), 500)
+	//	return
+	//}
+}
+
+func FindUser(w http.ResponseWriter, r *http.Request, name, pass string) *User {
+	row := db.QueryRow("SELECT * FROM users WHERE name=$1 AND password=$2", name, pass)
+
+	founded := new(User)
+	err := row.Scan(&founded.Id, &founded.Name, &founded.Password)
+	if err == sql.ErrNoRows {
+		return nil
+	} else if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return nil
+	}
+
+	return founded
+}
+
+func ShowAllUser() []*User {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	usrs := make([]*User, 0)
+	for rows.Next() {
+		us := new(User)
+		err := rows.Scan(&us.Id, &us.Name, &us.Password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		usrs = append(usrs, us)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, us := range usrs {
+		fmt.Printf("%d, %s, %s\n", us.Id, us.Name, us.Password)
+	}
+
+	return usrs
+}
